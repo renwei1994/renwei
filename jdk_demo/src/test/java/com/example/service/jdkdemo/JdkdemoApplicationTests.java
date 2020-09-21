42ab8d42cf043e492644344be6f5a89babad2675
renwei1994 <1396731147@qq.com>
2020-09-21 12:13:10 +0800

    dome

---
+++ jdk_demo/src/test/java/com/example/service/jdkdemo/JdkdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.service.jdkdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdkdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
