c7f158cb8a1d619e485a1a6b28f2d4bc62364aa9
renwei1994 <1396731147@qq.com>
2020-09-25 15:01:03 +0800

    jwt

---
+++ module_common/src/main/java/com/example/module_common/ModuleCommonApplication.java
@@ -0,0 +1,13 @@
+package com.example.module_common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleCommonApplication.class, args);
+    }
+
+}
