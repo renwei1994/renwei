9f450429c8dae734419388cf48043921580c2c6d
renwei1994 <1396731147@qq.com>
2020-09-25 17:43:25 +0800

    jwt

---
+++ module-service-renwei-system/src/main/java/com/example/moduleservicerenweisystem/ModuleServiceRenweiSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.moduleservicerenweisystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleServiceRenweiSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleServiceRenweiSystemApplication.class, args);
+    }
+
+}
---
+++ module-service-renwei-system/src/test/java/com/example/moduleservicerenweisystem/ModuleServiceRenweiSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moduleservicerenweisystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleServiceRenweiSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
