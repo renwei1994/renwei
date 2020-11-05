b4160e014c98236d70bc1b35e98d9818f67b48f6
renwei1994 <1396731147@qq.com>
2020-11-05 11:42:15 +0800

    线程

---
+++ module_datasource/src/main/java/com/renwei/module_datasource/ModuleDatasourceApplication.java
@@ -0,0 +1,13 @@
+package com.renwei.module_datasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleDatasourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleDatasourceApplication.class, args);
+    }
+
+}
---
+++ module_datasource/src/test/java/com/renwei/module_datasource/ModuleDatasourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renwei.module_datasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleDatasourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
