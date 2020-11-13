b7601d9eedb6be2f89ca3ca8bd5bdd54dd02c55b
renwei1994 <1396731147@qq.com>
2020-11-13 15:26:57 +0800

    线程池初步认识

---
+++ module_executor/src/main/java/com/example/module_executor/ModuleExecutorApplication.java
@@ -0,0 +1,13 @@
+package com.example.module_executor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleExecutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleExecutorApplication.class, args);
+    }
+
+}
