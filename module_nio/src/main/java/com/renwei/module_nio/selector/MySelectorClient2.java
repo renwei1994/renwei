31d199a1b9d2c2a6425224fea58e3a6c6d12dd2d
renwei1994 <1396731147@qq.com>
2020-12-03 11:08:56 +0800

    多路复用器

---
+++ module_nio/src/main/java/com/renwei/module_nio/selector/MySelectorClient2.java
@@ -0,0 +1,69 @@
+package com.renwei.module_nio.selector;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * <p>
+ * 服务实现类
+ * </p>
+ *
+ * @author renwei
+ * @since 2020/12/2 0002
+ */
+public class MySelectorClient2 {
+    private static Selector selector;
+
+    public static void main(String[] args) throws IOException {
+        selector = Selector.open();
+
+        SocketChannel socketChannel = SocketChannel.open();
+        socketChannel.configureBlocking(false);
+        socketChannel.connect(new InetSocketAddress("192.168.1.95", 8080));
+        socketChannel.register(selector, SelectionKey.OP_CONNECT);
+        while (true) {
+            System.out.println("客户端已准备就绪1............");
+            selector.select();
+            Set<SelectionKey> selectionKeys = selector.selectedKeys();
+            Iterator<SelectionKey> iterator = selectionKeys.iterator();
+            while (iterator.hasNext()) {
+                SelectionKey next = iterator.next();
+                iterator.remove();
+                if (next.isAcceptable()) {
+                    isAcceptable(next);
+                } else if (next.isConnectable()) {
+                    isAcceptable(next);
+                } else if (next.isReadable()) {
+                    isReadable(next);
+                } else if (next.isWritable()) {
+
+                }
+
+            }
+        }
+    }
+
+    public static void isAcceptable(SelectionKey selectionKey) throws IOException {
+        SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+        if (socketChannel.isConnectionPending()) {
+            socketChannel.finishConnect();
+        }
+        socketChannel.configureBlocking(false);
+        socketChannel.write(ByteBuffer.wrap("I am 客户端1  输入".getBytes()));
+        socketChannel.register(selector, SelectionKey.OP_READ);//监听读的时间
+    }
+
+
+    public static void isReadable(SelectionKey selectionKey) throws IOException {
+        SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        socketChannel.read(byteBuffer);
+        System.out.println("客户端1收到：" + new String(byteBuffer.array()));
+    }
+}
--- module_nio/src/main/java/com/renwei/module_nio/selector/MySelectorServer.java
+++ module_nio/src/main/java/com/renwei/module_nio/selector/MySelectorServer.java
@@ -1,9 +1,12 @@
 package com.renwei.module_nio.selector;
 
+import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -28,6 +31,7 @@ public class MySelectorServer {
         SelectionKey selectionKey = serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);//把当前通道注册到selector 监听链接事件
 
         while (true) {
+            System.out.println("服务端已准备就绪............");
             //当时间没到达之前  此方法会阻塞 阻塞所有注册到多路复用器上的事件
             //一旦某个事件处于就绪，那么就会返回
             selector.select();
@@ -35,21 +39,47 @@ public class MySelectorServer {
             Set<SelectionKey> selectionKeys = selector.selectedKeys();
             Iterator<SelectionKey> iterator = selectionKeys.iterator();
             while (iterator.hasNext()) {
+                System.out.println("you ji ge key");
                 SelectionKey next = iterator.next();
                 iterator.remove();//避免重复处理
                 if (next.isAcceptable()) {
-
+                    isAcceptable(next);
                 } else if (next.isConnectable()) {
 
                 } else if (next.isReadable()) {
-
+                    isReadable(next);
                 } else if (next.isWritable()) {
 
                 }
             }
-
         }
-
-
     }
+
+    /**
+     * 链接事件
+     *
+     * @param key
+     */
+    public static void isAcceptable(SelectionKey key) throws IOException {
+        ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key.channel();
+
+        //能走到这里socketChannel一定不为null
+        SocketChannel socketChannel = serverSocketChannel.accept();
+        socketChannel.configureBlocking(false);//io非阻塞
+        socketChannel.write(ByteBuffer.wrap("I am 服务端  输入".getBytes()));
+        socketChannel.register(selector, SelectionKey.OP_READ);//再把读事件注册上去
+    }
+
+    /**
+     * 读事件
+     *
+     * @param key
+     */
+    public static void isReadable(SelectionKey key) throws IOException {
+        SocketChannel socketChannel = (SocketChannel) key.channel();
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        socketChannel.read(byteBuffer);
+        System.out.println("服务端读到：" + new String(byteBuffer.array()));
+    }
+
 }
